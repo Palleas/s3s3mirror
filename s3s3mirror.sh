4e4fc54180ef2fd5c05c251abf8e1b8c04cd051b
Romain Pouclet <romain.pouclet@gmail.com>
2017-10-12 10:07:12 -0400

    Tweak path to log

--- s3s3mirror.sh
+++ s3s3mirror.sh
@@ -5,16 +5,16 @@ THISDIR=$(cd "$(dirname $0)" && pwd)
 VERSION=1.2.6
 JARFILE="${THISDIR}/target/s3s3mirror-${VERSION}-SNAPSHOT.jar"
 VERSION_ARG="-Ds3s3mirror.version=${VERSION}"
+LOGS_ARG="-Dlog4j.configuration=file:${THISDIR}/target/conf/log4j.xml"
 
 DEBUG=$1
 if [ "${DEBUG}" = "--debug" ] ; then
-  # Run in debug mode
   shift   # remove --debug from options
-  java -Dlog4j.configuration=file:target/conf/log4j.xml -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005 ${VERSION_ARG} -jar "${JARFILE}" "$@"
+  java ${LOGS_ARG} -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005 ${VERSION_ARG} -jar "${JARFILE}" "$@"
 
 else
   # Run in regular mode
-  java ${VERSION_ARG} -Dlog4j.configuration=file:target/conf/log4j.xml -jar "${JARFILE}" "$@"
+  java ${VERSION_ARG} ${LOGS_ARG} -jar "${JARFILE}" "$@"
 fi
 
 exit $?
